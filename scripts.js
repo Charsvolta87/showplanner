7a7cdd15822d79127d1a44d6faf318e77d3d44c3
Chars Volta <charsvolta@gmail.com>
2026-03-18 16:06:10 -0300

    dgzd

--- scripts.js
+++ scripts.js
@@ -137,3 +137,20 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+const searchInput = document.getElementById("searchInput");
+
+searchInput.addEventListener("keyup", function () {
+  const filter = searchInput.value.toLowerCase();
+  const cards = document.querySelectorAll(".ticket-card");
+
+  cards.forEach(card => {
+    const text = card.innerText.toLowerCase();
+
+    if (text.includes(filter)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+});
